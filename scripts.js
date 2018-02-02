901006c8af533241b846c8468cfe5826892ffece
Taylor Lea <tcl@deako.com>
2018-02-02 12:06:40 -0800

    add some comments

--- scripts.js
+++ scripts.js
@@ -1,8 +1,17 @@
 // Using namesList and addressesList, accomplish the objective listed in README.md
+
 console.log(
+	// map over namesList, returning a new Array
 	namesList.map((name) => {
-			return Object.assign(name, { 
-				"address": addressesList.find(address => address.id === name.id).address
+		/* 
+		for each name Object in namesList, add 
+		an "address" field with the corresponding 
+		address from addressList as the value.
+		*/
+			return Object.assign(name, {
+				"address": addressesList.find(
+								address => address.id === name.id
+							).address
 			});
 		}
 	)
